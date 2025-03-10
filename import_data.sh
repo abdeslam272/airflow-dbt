f90a8deab2871084200d04414bbd9c4d89dafb5e
abdeslam272 <abdeslam1999elbennouri@gmail.com>
2025-03-10 14:57:22 +0100

    ajouter les fichiers dbt

--- import_data.sh
+++ import_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "📂 Copying CSV files to PostgreSQL container..."
-docker cp products.csv postgres-dbt:/products.csv
-docker cp customers.csv postgres-dbt:/customers.csv
-docker cp orders.csv postgres-dbt:/orders.csv
-docker cp order_items.csv postgres-dbt:/order_items.csv
+docker cp data/products.csv postgres-dbt:/products.csv
+docker cp data/customers.csv postgres-dbt:/customers.csv
+docker cp data/orders.csv postgres-dbt:/orders.csv
+docker cp data/order_items.csv postgres-dbt:/order_items.csv
 
 echo "🚀 Running data import in PostgreSQL..."
 docker exec -i postgres-dbt psql -U dbt-user -d dbt-db -c "\copy raw.products FROM '/products.csv' WITH CSV HEADER;"
