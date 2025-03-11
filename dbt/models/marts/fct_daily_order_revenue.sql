4b83acf37cc43396f225f58bdd52caadaebbad10
abdeslam272 <abdeslam1999elbennouri@gmail.com>
2025-03-11 15:11:04 +0100

    ajouter un nouveau model

---
+++ dbt/models/marts/fct_daily_order_revenue.sql
@@ -0,0 +1,9 @@
+SELECT
+  order_date,
+  O.order_id,
+  sum(total_price) AS total_price
+FROM
+  {{ ref('stg_orders') }} O
+LEFT JOIN {{ ref('stg_order_items') }} OI
+ON O.order_id = OI.order_id
+GROUP BY 1,2
